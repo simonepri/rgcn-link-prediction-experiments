cf94e3f17c4f5b96f8cc7bc747f036c8e839158a
simonepri <simonepri@outlook.com>
2020-03-07 20:32:36 +0000

    Add ComplEx decoder

--- src/mocks/model_builder.py
+++ src/mocks/model_builder.py
@@ -1,7 +1,10 @@
+from mocks.decoders.complex import ComplEx
 from mocks.decoders.distmult import DistMult
 
 def build_decoder(encoder, decoder_settings):
     if decoder_settings["Name"] == "distmult":
         return DistMult(encoder, decoder_settings)
+    elif decoder_settings["Name"] == "complex":
+        return ComplEx(encoder, decoder_settings)
     else:
         return None
