b36e75472e0c9e3a3ca6533b42952b05a7cbca75
simonepri <simonepri@outlook.com>
2020-03-08 01:27:38 +0000

    Add seed argument to the train script

--- src/bin/train.py
+++ src/bin/train.py
@@ -1,9 +1,38 @@
 #!/usr/bin/python3
+import argparse
 import os
+import random
 import sys
 import unittest
 
+import numpy as np
+import tensorflow as tf
+
 from mocks.model_builder import build_decoder
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Train a model on a given dataset."
+    )
+    parser.add_argument(
+        "--settings",
+        type=str,
+        required=True,
+        help="Filepath for settings file.",
+    )
+    parser.add_argument(
+        "--dataset", type=str, required=True, help="Filepath for dataset."
+    )
+    parser.add_argument("--seed", type=int, default=42, help="Random seed.")
+    args = parser.parse_args()
+    return args
+
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.compat.v1.set_random_seed(seed)
 
 
 def run_in_submodule(function):
@@ -17,13 +46,24 @@ def run_in_submodule(function):
 
 def run_mocked_train():
     @unittest.mock.patch("common.model_builder.build_decoder", build_decoder)
-    def run():
+    def run(args):
         # Disable most of the TF warnings
         os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+        # Set random seed for riproducibility
+        set_seed(args.seed)
+        # Set arguments
+        sys.argv = [
+            "src/submodules/rgcn/code/train.py",
+            "--settings",
+            args.settings,
+            "--dataset",
+            args.dataset,
+        ]
         # pylint: disable=import-outside-toplevel,unused-import
         import train
 
-    run()
+    ARGS = parse_args()
+    run(ARGS)
 
 
 run_in_submodule(run_mocked_train)
